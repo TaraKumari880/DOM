d12d017c03e1399ea54320ba213e5a495b718371
Tara Kumari <tararani88099@gmail.com>
2024-02-28 22:11:14 +0530

    file added

---
+++ script.js
@@ -0,0 +1,8 @@
+let h3 = document.getElementById("text");
+//console.log(h3);
+h3.style.color ="blue";
+h3.style.backgroundColor ="yellow";
+h3.style.height = "300px";
+h3.style.width = "400px";
+h3.style.padding = "20px";
+h3.style.margin = "30px";
